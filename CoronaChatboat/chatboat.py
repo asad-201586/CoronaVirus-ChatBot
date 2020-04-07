9c06b5661a3af8370da9516cb80dc00d2a10c719
Md Asadul Haque <asadul201586@gmail.com>
2020-04-08 02:27:57 +0600

    Add files via upload
    Corona ChatBot Chatting Window

---
+++ CoronaChatboat/chatboat.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template,jsonify,request
+import model
+
+
+app = Flask(__name__)
+app.secret_key = '12345'
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+
+@app.route('/chat',methods=["POST"])
+def chat():
+    try:
+        user_message = request.form["text"] #user text eikhane paben
+        response_text = model.chat(user_message) #eikhane chat bot er response ta diben
+        return jsonify({"status":"success","response":response_text})
+    except Exception as e:
+        print(e)
+        return jsonify({"status":"success","response":"Sorry I am not trained to do that yet..."})
+
+if __name__ == "__main__":
+    app.run(port=5002)
